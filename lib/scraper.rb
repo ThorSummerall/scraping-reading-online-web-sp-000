5b63e8515ab2686a07d7e5bcd893995cc3f0d9af
Thor Summerall <thor.summerall@gmail.com>
2020-12-16 16:18:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-
-courses = doc.css(".id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-
-courses.each do |course|
-  puts course.text.strip
-end
+puts doc.css(".headline-26OIBN").text
